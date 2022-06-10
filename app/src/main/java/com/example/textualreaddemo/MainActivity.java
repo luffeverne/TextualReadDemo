60749cb15636595e2d706ca07a059c0ce8505c7f
houdeng <1960319433@qq.com>
2022-06-10 17:49:09 +0800

    微调主题

--- app/src/main/java/com/example/textualreaddemo/MainActivity.java
+++ app/src/main/java/com/example/textualreaddemo/MainActivity.java
@@ -9,6 +9,7 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
