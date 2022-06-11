063d22fcc6074c2a5c4f154e756453f04f590b33
Luffe <luffevernecute@gmail.com>
2022-06-11 11:33:41 +0800

    改了点登录界面，没做完

--- app/src/main/java/com/example/textualreaddemo/MainActivity.java
+++ app/src/main/java/com/example/textualreaddemo/MainActivity.java
@@ -1,35 +1,22 @@
 package com.example.textualreaddemo;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelStoreOwner;
-import androidx.viewpager2.widget.ViewPager2;
-
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.textualreaddemo.R;
-import com.example.textualreaddemo.basebean.newsdata.NewsListBean;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
 import com.example.textualreaddemo.homepage.MainViewAdapter;
 import com.example.textualreaddemo.homepage.MyPageFragment;
 import com.example.textualreaddemo.homepage.NewsListPageFragment;
 import com.example.textualreaddemo.homepage.SearchPageFragment;
-import com.example.textualreaddemo.network.HttpUtil;
-import com.example.textualreaddemo.network.NewsDataUtility;
-
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Response;
 
 /**
- * 主页
+ * 主页登录跳转
  * 最后更改时间：2022-6-10 13:58
  */
 
@@ -37,6 +24,12 @@ public class MainActivity extends AppCompatActivity{
 
     private ViewPager2 mainView;
     private MainViewAdapter adapter;
+
+    private TextView mButtonLogin;
+    private TextView mButtonRegister;
+    private TextView mButtonForgetPassword;
+
+    private TextView mEditUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +52,52 @@ public class MainActivity extends AppCompatActivity{
         adapter.addFragment(new NewsListPageFragment());
         adapter.addFragment(new SearchPageFragment());
         mainView.setCurrentItem(1);
+
+        // 找控件
+        initView();
+        
+        // 设置点击事件
+        initClickEvent();
+
+    }
+
+    private void initClickEvent() {
+//        mEditUserName = findViewById(R.id.username);
+
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println(mEditUserName.getText());
+            }
+        });
+
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("点击了新用户注册");
+                // 跳转到新用户注册界面
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+        mButtonForgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("点击了忘记密码");
+                // 跳转到重置密码界面
+            }
+        });
+
+    }
+
+    private void initView() {
+        mButtonLogin = this.findViewById(R.id.btn_login);
+        mButtonRegister = this.findViewById(R.id.btn_register);
+        mButtonForgetPassword = this.findViewById(R.id.btn_forgetPassword);
+        mEditUserName = this.findViewById(R.id.username);
 
     }
 
