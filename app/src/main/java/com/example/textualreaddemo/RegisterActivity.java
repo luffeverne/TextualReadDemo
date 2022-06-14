bf392dca3d2c4a5de12bdec2736a88ccebf7264b
Luffe <luffevernecute@gmail.com>
2022-06-14 21:43:42 +0800

    注册页更新，bug未解决”

--- app/src/main/java/com/example/textualreaddemo/RegisterActivity.java
+++ app/src/main/java/com/example/textualreaddemo/RegisterActivity.java
@@ -2,29 +2,38 @@ package com.example.textualreaddemo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.textualreaddemo.data.UserData;
+import com.example.textualreaddemo.data.Code;
+import com.example.textualreaddemo.data.DBOpenHelper;
 
 
 /**
  * 注册界面
- * 最后更改时间：2022-6-11 17:00
+ * 最后更改时间：2022-6-14 17:00
  * author: luffe
  */
 
 
-public class RegisterActivity extends AppCompatActivity {
-    private TextView mButtonRegister;
-    private TextView mButtonCancel;
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String strShowCode;
+    private DBOpenHelper mDBOpenHelper;
+    private Button mButtonRegister;
+    private Button mButtonCancel;
     private EditText mEditUsername;
     private EditText mEditPassword;
     private EditText mEditConfirmPassword;
+    private ImageView mIvShowCode;
+    private EditText mEditInputCode;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,60 +41,66 @@ public class RegisterActivity extends AppCompatActivity {
         // 调用 layout 里面的 activity_register.xml
         setContentView(R.layout.activity_register);
 
-        // 设置点击事件
-        initClickEvent();
+        initView();
+
+        mDBOpenHelper = new DBOpenHelper(this);
+
+        // 将验证码用图片的形式显示出来
+        mIvShowCode.setImageBitmap(Code.getInstance().createBitmap());
+        strShowCode = Code.getInstance().getCode().toLowerCase();
     }
 
-    private void initClickEvent() {
-        // 点击 注册 按钮
-        mButtonRegister = this.findViewById(R.id.btn_register);
-        mButtonRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 获取账号名、密码、确认密码
-                mEditUsername = findViewById(R.id.text_username);
-                mEditPassword = findViewById(R.id.text_password);
-                mEditConfirmPassword = findViewById(R.id.text_confirmPassword);
+    private void initView() {
+        // 初始化控件
+        mButtonRegister = findViewById(R.id.btn_register);
+        mButtonCancel = findViewById(R.id.btn_cancel);
+        mIvShowCode = findViewById(R.id.btn_showCode);
+        mEditUsername = findViewById(R.id.text_username);
+        mEditPassword = findViewById(R.id.text_password);
+        mEditConfirmPassword = findViewById(R.id.text_confirmPassword);
+        mEditInputCode = findViewById(R.id.text_InputCode);
 
-                String username = mEditUsername.getText().toString();
-                String password = mEditPassword.getText().toString();
-                String confirmPassword = mEditConfirmPassword.getText().toString();
-                UserData user = new UserData();
+        // 设置点击事件监听器
+        mButtonRegister.setOnClickListener(this);
+        mButtonCancel.setOnClickListener(this);
+    }
 
-                if (password.equals(confirmPassword)) {
-                    // 存储账户密码
-                    user.setUserName(username);
-                    user.setPassword(password);
 
-                    // 传回账号
-                    Intent intent = new Intent();
-                    intent.putExtra("mEditUsername", username);
-                    setResult(RESULT_OK, intent);
-
-                    // 返回登录页
-                    Intent loginPage = new Intent(RegisterActivity.this, LoginActivity.class);
-                    startActivity(loginPage);
-                    onDestroy();
-
-                    finish();
-
-                } else {
-                    Toast.makeText(RegisterActivity.this, "两次密码不一样", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        // 点击 取消 按钮
-        mButtonCancel = this.findViewById(R.id.btn_cancel);
-        mButtonCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 返回登录页
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_cancel: // 取消注册，返回登录页
                 Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
-                onDestroy();
-            }
-        });
+                finish();
+                break;
+            case R.id.btn_showCode: // 更新随机验证码生成
+                mIvShowCode.setImageBitmap(Code.getInstance().createBitmap());
+                strShowCode = Code.getInstance().getCode().toLowerCase();
+                break;
+            case R.id.btn_register: // 注册
+                // 获取用户输入的用户名、密码
+                String username = mEditUsername.getText().toString().trim();
+                String password = mEditPassword.getText().toString().trim();
+                String confirmPassword = mEditConfirmPassword.getText().toString().trim();
+                String inputCode = mEditInputCode.getText().toString().trim();
 
+                // 注册验证
+                if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(inputCode)) {
+                    if (!(password == confirmPassword)) {
+                        // 将用户名和密码加入到数据库中
+                        mDBOpenHelper.add(username, confirmPassword);
+                        // 注册成功了，进入主页
+                        Intent intent1 = new Intent(RegisterActivity.this, MainActivity.class);
+                        startActivity(intent1);
+                        finish();
+                        Toast.makeText(RegisterActivity.this, "注册成功！", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "密码输入不一致，请重新输入！", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+        }
     }
+
 }
