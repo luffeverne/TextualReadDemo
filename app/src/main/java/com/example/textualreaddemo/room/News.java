3dd196defb6d45f76e9f4fb0ab32d672a8a85df9
kun <2090938738@qq.com>
2022-06-18 08:57:44 +0800

    kunRoomFirst

---
+++ app/src/main/java/com/example/textualreaddemo/room/News.java
@@ -0,0 +1,99 @@
+package com.example.textualreaddemo.room;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class News {
+    @PrimaryKey()
+    @ColumnInfo(name = "newsID",typeAffinity = ColumnInfo.TEXT)
+    @NonNull
+    private String newsID;
+
+    @ColumnInfo(name = "userID",typeAffinity = ColumnInfo.TEXT)
+    private String userID;
+
+    @ColumnInfo(name = "isLike",typeAffinity = ColumnInfo.TEXT)
+    private String isLike;
+
+    @ColumnInfo(name = "isCollected",typeAffinity = ColumnInfo.TEXT)
+    private String isCollected;
+
+    @ColumnInfo(name = "isCommented",typeAffinity = ColumnInfo.TEXT)
+    private String isCommented;
+
+    @ColumnInfo(name = "isHistory",typeAffinity = ColumnInfo.TEXT)
+    private String isHistory;
+
+    public News(@NonNull String newsID, String userID, String isLike, String isCollected, String isCommented, String isHistory) {
+        this.newsID = newsID;
+        this.userID = userID;
+        this.isLike = isLike;
+        this.isCollected = isCollected;
+        this.isCommented = isCommented;
+        this.isHistory = isHistory;
+    }
+
+    @NonNull
+    public String getNewsID() {
+        return newsID;
+    }
+
+    public void setNewsID(@NonNull String newsID) {
+        this.newsID = newsID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getIsLike() {
+        return isLike;
+    }
+
+    public void setIsLike(String isLike) {
+        this.isLike = isLike;
+    }
+
+    public String getIsCollected() {
+        return isCollected;
+    }
+
+    public void setIsCollected(String isCollected) {
+        this.isCollected = isCollected;
+    }
+
+    public String getIsCommented() {
+        return isCommented;
+    }
+
+    public void setIsCommented(String isCommented) {
+        this.isCommented = isCommented;
+    }
+
+    public String getIsHistory() {
+        return isHistory;
+    }
+
+    public void setIsHistory(String isHistory) {
+        this.isHistory = isHistory;
+    }
+
+    @Override
+    public String toString() {
+        return "News{" +
+                "newsID=" + newsID +
+                ", userID=" + userID +
+                ", isLike=" + isLike +
+                ", isCollected=" + isCollected +
+                ", isCommented=" + isCommented +
+                ", isHistory=" + isHistory +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/textualreaddemo/room/UserDao.java
@@ -0,0 +1,36 @@
+package com.example.textualreaddemo.room;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+
+    @Insert
+    void insertUsers(User... users);
+
+    @Query("Delete From User Where userID = :userID")
+    void deleteUserByUserID(String userID);
+
+    @Query("Delete From User Where name = :userName")
+    void deleteUserByUserName(String userName);
+
+    @Update
+    void updateUsers(User... users);
+
+    @Query("select * From User Where userID = (:userID)")
+    User getUserByUserID(String userID);
+
+    @Query("select * From User Where name = :name")
+    User getUserByName(String name);
+
+    @Query("Delete From user")
+    void deleteAllUsers();
+
+    @Query("Select * From user Order By userID Desc")
+    List<User> getAllUsers();
+}
